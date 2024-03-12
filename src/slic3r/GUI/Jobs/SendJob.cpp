562232c1400f82e763cfbfb72aaab852456b8113
Inusha <inusha12345@gmail.com>
2024-03-12 00:35:31 -0230

    Add getlogin() to project_name in SendJob.cpp

--- src/slic3r/GUI/Jobs/SendJob.cpp
+++ src/slic3r/GUI/Jobs/SendJob.cpp
@@ -5,6 +5,7 @@
 #include "slic3r/GUI/Plater.hpp"
 #include "slic3r/GUI/GUI.hpp"
 #include "slic3r/GUI/GUI_App.hpp"
+#include <unistd.h> // for getlogin()
 
 namespace Slic3r {
 namespace GUI {
@@ -199,7 +200,7 @@ void SendJob::process()
         return;
     }
 
-    std::string project_name = wxGetApp().plater()->get_project_name().ToUTF8().data();
+    std::string project_name = std::string(getlogin()) + " - " + m_project_name + ".gcode.3mf";
     int curr_plate_idx = 0;
     if (job_data.plate_idx >= 0)
         curr_plate_idx = job_data.plate_idx + 1;
@@ -207,7 +208,7 @@ void SendJob::process()
         curr_plate_idx = m_plater->get_partplate_list().get_curr_plate_index() + 1;
 
     params.dev_id               = m_dev_id;
-    params.project_name         = m_project_name + ".gcode.3mf";
+    params.project_name         = project_name;
     params.preset_name          = wxGetApp().preset_bundle->prints.get_selected_preset_name();
     params.filename             = job_data._3mf_path.string();
     params.config_filename      = job_data._3mf_config_path.string();
